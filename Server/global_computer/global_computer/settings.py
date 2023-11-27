4a2a59961aa8f24eae94bfe36217cadb5d18023a
Jahidul Islam <jahidul.islam.sec@gmail.com>
2023-11-27 17:38:40 +0600

    update to postgres

--- Server/global_computer/global_computer/settings.py
+++ Server/global_computer/global_computer/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'global_computer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
