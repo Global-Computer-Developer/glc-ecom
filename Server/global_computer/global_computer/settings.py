675a5595ac4208d3d128dad4ea871381158c23c5
Jahidul Islam <jahidul.islam.sec@gmail.com>
2023-11-28 15:07:14 +0600

    reverse the settings

--- Server/global_computer/global_computer/settings.py
+++ Server/global_computer/global_computer/settings.py
@@ -28,25 +28,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wpx$@io&-dzyks&m=z%&uc=4b!qn*@6no$(^a(!qivf2jypr#z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-### ------ DJANGO APP SECURITY -------##
-# Cross-site Scripting for deploy
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
+# ### ------ DJANGO APP SECURITY -------##
+# # Cross-site Scripting for deploy
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
 
-# SSL redirect
-SECURE_SSL_REDIRECT = True
+# # SSL redirect
+# SECURE_SSL_REDIRECT = True
 
-# HTTP Strict Transport Security
-SECURE_HSTS_SECONDS = 86400
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# # HTTP Strict Transport Security
+# SECURE_HSTS_SECONDS = 86400
+# SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-#  Cross-site request forgery (CSRF) protection
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# #  Cross-site request forgery (CSRF) protection
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 ### ------ END DJANGO APP SECURITY -------##
 
