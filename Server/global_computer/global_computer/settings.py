bb8ea32f19d0cc02392580cce899eb9b4e6a288a
Jahidul Islam <jahidul.islam.sec@gmail.com>
2023-12-06 11:11:14 +0600

    fresh start

--- Server/global_computer/global_computer/settings.py
+++ Server/global_computer/global_computer/settings.py
@@ -138,7 +138,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'global_ecom',
-        'USER': 'jahidul',
+        'USER': 'root',
         'PASSWORD': 'J@hidul42598475',
         'HOST': 'localhost',
         'PORT': '3306',
@@ -198,11 +198,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-
-
 STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
